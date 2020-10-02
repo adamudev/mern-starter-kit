2e60a5b76df47ae3f5549f4ba1df294f7fb30a3d
Adamu Abubakar <adamudev@gmail.com>
2020-10-02 02:26:51 +0100

    add client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>There you have it. Do your thing.✌</h1>
+    </div>
+  );
+}
+
+export default App;
